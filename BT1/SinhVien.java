6d95ca02ea6a20469a6aa33c2e8318051a7c7aa7
CuongTrang-13 <trang1312006.a1.k46@gmail.com>
2025-09-10 14:48:23 +0700

    btap

--- BT1/SinhVien.java
+++ BT1/SinhVien.java
@@ -1,38 +1,37 @@
 public class SinhVien {
-    // 1. Thuộc tính đối tượng (instance variables)
+    
     private String maSv;
     private String hoTen;
     private int tuoi;
 
-    // 2. Thuộc tính tĩnh (static variable)
+    
     private static int soLuong = 0;
 
-    // Constructor
+    
     public SinhVien() {
-        soLuong++; // Mỗi lần tạo sinh viên thì tăng số lượng
+        soLuong++; 
     }
 
-    // 3. Phương thức
-    // setThongTin
+    
+   
     public void setThongTin(String maSv, String hoTen, int tuoi) {
         this.maSv = maSv;
         this.hoTen = hoTen;
         this.tuoi = tuoi;
     }
 
-    // hienThiThongTin
     public void hienThiThongTin() {
         System.out.println("Mã SV: " + maSv);
         System.out.println("Họ tên: " + hoTen);
         System.out.println("Tuổi: " + tuoi);
     }
 
-    // hienThiSoLuong (static)
+    
     public static void hienThiSoLuong() {
         System.out.println("Tổng số sinh viên: " + soLuong);
     }
 
-    // Main để test
+   
     public static void main(String[] args) {
         SinhVien sv1 = new SinhVien();
         sv1.setThongTin("SV001", "Nguyen Van A", 20);
